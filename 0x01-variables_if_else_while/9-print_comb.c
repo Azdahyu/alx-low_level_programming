a59b7fc311a66dfababa86a1936a0275540cd93c
Augustina <tina@example.com>
2023-08-11 19:31:44 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: this is always 0
+ */
+
+int main(void)
+{
+	int t;
+
+	for (t = 0 ; t < 10 ; t++)
+	{
+		putchar(t);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
