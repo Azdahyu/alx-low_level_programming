f136d1a152517eb221aa8f852209ed8665a4bed3
Augustina <tina@example.com>
2023-08-15 21:13:35 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - this function adds two numbers
+ * @a: this is the first operand
+ * @b: this is the second operand
+ * Return: this returns the addition of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,6 @@ void times_table(void)
 					printf(", ");
 			}
 		}
+		printf("\n");
 	}
-	printf("\n");
 }
